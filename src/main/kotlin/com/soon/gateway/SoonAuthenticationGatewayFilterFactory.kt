032056432c16d2f7c9a4bf81e66e342940b06b05
chs98412 <chs98412@gmail.com>
2024-09-09 20:31:58 +0900

    initial setting && cicd

--- src/main/kotlin/com/soon/gateway/SoonAuthenticationGatewayFilterFactory.kt
+++ src/main/kotlin/com/soon/gateway/SoonAuthenticationGatewayFilterFactory.kt
@@ -1,19 +1,19 @@
-import org.springframework.cloud.gateway.filter.GatewayFilter
-import org.springframework.cloud.gateway.filter.GatewayFilterChain
-import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
-import org.springframework.stereotype.Component
-import org.springframework.web.server.ServerWebExchange
-
-@Component("SoonAuthentication")
-class SoonAuthenticationGatewayFilterFactory : AbstractGatewayFilterFactory<Any>() {
-    override fun apply(config: Any?): GatewayFilter {
-        println("SoonAuthentication filter is executed")
-        return GatewayFilter { exchange, chain ->
-            chain.filter(exchange)
-        }
-    }
-
-    class Config {
-        // Configuration class content
-    }
-}
+//import org.springframework.cloud.gateway.filter.GatewayFilter
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain
+//import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+//import org.springframework.stereotype.Component
+//import org.springframework.web.server.ServerWebExchange
+//
+//@Component
+//class SoonAuthenticationGatewayFilterFactory : AbstractGatewayFilterFactory<Any>() {
+//    override fun apply(config: Any?): GatewayFilter {
+//        println("SoonAuthentication filter is executed")
+//        return GatewayFilter { exchange, chain ->
+//            chain.filter(exchange)
+//        }
+//    }
+//
+//    class Config {
+//        // Configuration class content
+//    }
+//}
